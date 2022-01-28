68f6423935aaf0d417a7a003a9fa82b1abacbc31
Anderson Nascimento <andersonsilva.nascimento98@gmail.com>
2022-01-28 20:03:28 -0300

    criado a validação do formulário

--- assents/src/form.js
+++ assents/src/form.js
@@ -7,13 +7,19 @@ function teste() {
   let url = new URL(window.location.href)
   let id = url.search.replace('?', '')
 
-
+  xp == null ? xp = '': false
+ 
+  validaForm(nome,tel,xp) 
+ 
+  
   let valores = {
         nome: nome.value,
         tel: tel.value,
         xp: xp.value
     }
+  //console.log(xp.value)
     
+  
     let dados = JSON.parse(localStorage.getItem("Cadastro"))
     
   if (dados == null) {
@@ -22,9 +28,7 @@ function teste() {
   }
 
 
-  if (xp.checked) {  
-     return validaForm()
-  }
+
   
   if (id == '') {
    dados.push(valores)    
@@ -32,24 +36,59 @@ function teste() {
     dados.splice(id, 1, valores)
   }
     
-  localStorage.setItem('Cadastro', JSON.stringify(dados))  
-  window.location.href = '../../index.html'
+  //localStorage.setItem('Cadastro', JSON.stringify(dados))  
+  //window.location.href = '../../index.html'
 
 }
 
 
 
 
-function validaForm() {
-  let x = document.querySelector('input[name="client"]')
-  let divErro =  document.createElement("div")
-  let texto  = document.createTextNode("Favor selecionar uma das opções acima!");
-  divErro.appendChild(texto)  
-  document.getElementById('xp').appendChild(divErro)
-  divErro.style.color = 'red'
-  divErro.style.fontSize = '16px'
-  divErro.style.textAlign = 'center'
-   
+function validaForm(nome,tel,xp) {
+  let nomeErro = document.querySelector('#areaNome')
+  let telErro = document.querySelector('#areaTel')
+  let xpErro = document.querySelector('#xp')
+  let regexNome = /([^a-zA-Z][\W]+)/.test(nome.value)
+  //regexNome.trim()
+   //ira verificar se o nome começa com numeros e buscara caracteres especiais, o trim() remove espaços desnecessarios
+  let regexTel = /[^0-9]+/.test(tel.value)
+
+
+  if (!regexNome) {
+    msgErro("oops!", nomeErro)
+    console.log(regexNome)
+  }
+   if (!regexTel) {
+    msgErro("Favor selecionar uma das opções acima!", telErro)
+  }
+   if (!xp.value) {
+    msgErro("Favor selecionar uma das opções acima!", xpErro)
+  } 
 }
 
+
+function msgErro(msg, divErro) {
+  let criaDiv =  document.createElement("div")
+  let texto = document.createTextNode(msg)
+  console.log(divErro)
+  
+
+  if (divErro.lastChild.classList) {
+    document.querySelector('.novaDiv').remove()
+
+  }
+    
+    criaDiv.appendChild(texto)  
+    divErro.appendChild(criaDiv)
+    criaDiv.classList.add('novaDiv')
+    criaDiv.style.color = 'red'
+    criaDiv.style.fontSize = '16px'
+    criaDiv.style.textAlign = 'center'
+    return false
+}
+
+
+
+  
  
+  console.log(/^[^a-zA-Z]/.test('9ae5- sf.'))
